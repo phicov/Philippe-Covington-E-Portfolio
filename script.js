dd2c504fcad174c88988800b64dc12f4ccb817d1
Philippe Covington <philippehcbusi@gmail.com>
2023-03-01 14:41:14 -0800

    Form Functionality!!!!!!!!!!!

---
+++ script.js
@@ -0,0 +1,25 @@
+// 
+// 
+// 
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading')
+  const success = document.querySelector('.modal__overlay--success')
+  loading.classList += ' modal__overlay--visible'
+
+  emailjs
+    .sendForm(
+      "service_b17ey0n",
+      "template_jxbvxpq",
+      event.target,
+      "QiS7h_2E4oYTOVOzv"
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible")
+      success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+      loading.classList.remove("modal_-overlay--visible");
+      alert(
+        "The email service is temporarily unavaible. Please contact me directly @ philippehcbusi@gmail.com"
+      )
+    })
+}
