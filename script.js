ac0ae0ab75185887fdd3b79b53c7c2372d3a25ba
Philippe Covington <philippehcbusi@gmail.com>
2023-03-02 16:01:50 -0800

    Modal Open/Close/ Animations

--- script.js
+++ script.js
@@ -24,4 +24,13 @@ function contact(event) {
     })
 }
 
-
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove('modal--open')
+  }
+  isModalOpen = true;
+  // toggle modal
+  document.body.classList += ' modal--open'
+}
